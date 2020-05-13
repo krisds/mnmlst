38eaef2145086bdf4a9517636ce57091ae15e1fb
krisds <kris.deschutter@gmail.com>
2020-05-13 19:26:01 +0200

    DFNTLY: updated version

--- dfntly/sample.hello_world.js
+++ dfntly/sample.hello_world.js
@@ -2,25 +2,22 @@
 //
 // This is a very simple example which prints some greetings.
 
-var requirejs = require('requirejs')
+let requirejs = require('requirejs')
 requirejs(['dfntly'], function(dfntly) {
 
   // This creates a node holding a name.
-  var name = dfntly('world')
+  let name = dfntly.is('world')
   
   // This creates a node which generates the greeting. Note that it references
   // the previous node in its calculation. That is enough for a link to get
   // established between both nodes.
-  var greeting = dfntly(function() {
-    return 'Hello ' + name() + '.'
-  })
+  let greeting = dfntly.is(() => 'Hello ' + name.value() + '.')
 
-  // When defining nodes based on functions the functions will get evaluated
-  // immediately. This fact will become obvious when running the code for the
-  // following definition. It prints the message. Because it gets evaluated
-  // immediately we should see the "Hello world." message on the console.
-  var print_it = dfntly(function() {
-    console.log(greeting())
+  // This node will log the greeting on the console. This will not happen
+  // immediately, as the dataflow engine will have to wait its turn to be run
+  // by the platform. Which is why you won't see a "Hello world." message.
+  let print_it = dfntly.is(() => {
+    console.log(greeting.value())
   })
 
   // The following statements update the name twice. Once to "Once-ler", then
@@ -28,10 +25,15 @@ requirejs(['dfntly'], function(dfntly) {
   // update all other nodes. However, due to the way the engine gets scheduled,
   // the recalculation will only trigger after both statements have executed.
   // This means that the "greeting" and "print_it" nodes will only get
-  // recalculated once each, and so we will only see one more message on the
+  // recalculated once each, and so we will only see one message on the
   // console. Namely "Hello Lorax."
-  name('Once-ler')
-  name('Lorax')
+  name.becomes('Once-ler')
+  name.becomes('Lorax')
+  
+  // To get another message printed we need to schedule the update of the name.
+  // We'll use `setImmediate` for that. The dataflow engine should already be
+  // scheduled in the same way, and take precedence. Once it has completed this
+  // will update the name, which will reschedule the engine and do another
+  // round of calculations.
+  setImmediate(() => { name.becomes("Dr. Seuss") })
 })
-
-
--- dfntly/sample.sqrt.js
+++ dfntly/sample.sqrt.js
@@ -5,50 +5,52 @@
 // I'm basing this off [this example program](http://www.macs.hw.ac.uk/~pjbk/pathways/cpp1/node124.html);
 // check that for more background information.
 
-var requirejs = require('requirejs')
+let requirejs = require('requirejs')
 requirejs(['dfntly'], function(dfntly) {
 
   // This is the number we'll be calculating the square root for.
-  var number = dfntly(77)
+  let number = dfntly.is(100)
 
   // The approximation needs an initial guess of the square root. I've
   // arbitrarily chosen one third of the initial value to be a good enough
   // guess.
-  var guess = dfntly(function() {
-    var value = number()/3
+  let guess = dfntly.is(() => {
+    let value = number.value()
+    let guess = value/3
 
     console.log(
-      '[' + number() + '] Initial guess: ' + value
-      + ' (Squared: ' + (value*value) + ')')
+      '[' + value + '] Initial guess: ' + guess
+      + ' (Squared: ' + (guess*guess) + ')')
         
-    return value
+    return guess
   })
 
   // The approximation of the square root repeatedly keeps improving on the
   // initial guess through a simple calculation.
-  var approximate = dfntly(function() {
-    return (guess() + number()/guess())/2
-  })
+  let approximate = dfntly.is(() => (guess.value() + number.value()/guess.value())/2)
 
   // If the current guess is a close enough approximation of the square root
   // we show the final answer. If not we update the value for the guess with
   // the latest approximation. This will trigger the dataflow engine again
   // for another round of approximating the square root.
-  var iterate = dfntly(function() {
-    var value = approximate()
+  let iterate = dfntly.is(() => {
+    let value = approximate.value()
     
-    if (Math.abs(number() - value*value) < 0.000001)
+    if (Math.abs(number.value() - value*value) < 0.000000001)
       console.log(
-        '[' + number() + '] Final answer: ' + value
-        + ' (Math.sqrt: ' + Math.sqrt(number()) + ')')
+        '[' + number.value() + '] Final answer: ' + value
+        + ' (Math.sqrt: ' + Math.sqrt(number.value()) + ')')
     else {
       console.log(
-        '[' + number() + '] Current guess: ' + value
+        '[' + number.value() + '] Current guess: ' + value
         + ' (Squared: ' + (value*value) + ')')
 
-      guess(approximate())
+      guess.becomes(approximate.value())
     }
   })
+  
+  // We can ask for the square root of another number by changing the value of
+  // the node. Note that we must schedule this for later, so that the engine
+  // has a chance of calculating the root of the first number first.
+  setImmediate(() => { number.becomes(77) })
 })
-
-
